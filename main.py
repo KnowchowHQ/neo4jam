22c8ea9af54f57d68dcc88ba33ec8ad1e890534a
Safwan Ahmad <safwan.pico@gmail.com>
2025-01-30 19:50:11 +0800

    Addvaluation handler to run evaluation module and save results.

--- main.py
+++ main.py
@@ -11,11 +11,13 @@ from typing import Union
 from time import sleep
 from drivers.loadenv import load_env
 from loguru import logger
+import benchmark.evaluationmetrics as metrics
+import csv
+
+CYPHER_QUERY_PAIRS = []
 
 
-def generate_cypher_queries(data: DataFrame, prompt: Union[UserPrompt, SystemPrompt]) -> dict:
-
-    cypher_query_pairs = {}
+def generate_cypher_queries(data: DataFrame, prompt: Union[UserPrompt, SystemPrompt], model: GenerativeModel) -> dict:
     
     # Build prompts to generate required Cypher queries
     for index, row in data.iterrows():
@@ -42,25 +44,48 @@ def generate_cypher_queries(data: DataFrame, prompt: Union[UserPrompt, SystemPro
                 "Schema": str(user_prompt['input_data'])
             }
         }
-
         response = model.generate_content(str(message_user))
-        cypher_query_pairs.update(
+        CYPHER_QUERY_PAIRS.append(
             {
                 "nlp_query": user_prompt["context"],
                 "real": ground_truth,
                 "generated": str(response.text).replace("\n", " ").replace("```cypher", " ")
             }
         )
+        
+        if index == 90:
+            return
+        if (index+1) % 15 == 0:
+            # Evaluate generated cyphers
+            evaluate_generated_cyphers(index)
+            sleep(60)
+            
 
-        sleep(5)
-
-        logger.info(cypher_query_pairs)
+        
 
 def configure_llm() -> GenerativeModel:
     # Configure Gemini API
     genai.configure(api_key=os.getenv("GEMINI"))
     model = genai.GenerativeModel("gemini-1.5-flash")
     return model
+
+
+def evaluate_generated_cyphers(index: int) -> None:
+    current_index = index - 14
+    if len(CYPHER_QUERY_PAIRS) != 0:
+        # Find the rouge scores
+        rouge_metric_factory = metrics.ROUGEMetricFactory()
+
+        try:
+            for i, data in enumerate(start=current_index, iterable=CYPHER_QUERY_PAIRS[current_index:]):
+                score =metrics.evaluate_model(factory=rouge_metric_factory, predictions=[data['generated']], references=[data['real']])
+                logger.info(score)
+                CYPHER_QUERY_PAIRS[i]['score'] = score
+                logger.info(CYPHER_QUERY_PAIRS[i])
+        except IndexError as ie:
+            return
+    else:
+        logger.info("No data available for cypher evaluation!")
 
 
 def execution_loop():
@@ -75,7 +100,15 @@ def execution_loop():
     logger.info(data.head)
 
     # Generate cypher queries
-    generate_cypher_queries(data, UserPrompt())
+    generate_cypher_queries(data=data, prompt=UserPrompt(), model=model)
+
+    # Save results
+    with open("result.csv", 'w') as file:
+        wr = csv.writer(file)
+        wr.writerow(["nlp_query", "real", "generated", "score"])
+
+        for row in CYPHER_QUERY_PAIRS:
+            wr.writerow([row["nlp_query"], row["real"], row["generated"], str(row["score"])])
 
 
 if __name__ == "__main__":
