d428b2f9468925245e9a2994242c2c5ca1e8dcdf
Farhan Ahmad <farhanhubble@gmail.com>
2025-02-10 23:24:09 +0800

    FEAT: Preprocessing data

--- cli.py
+++ cli.py
@@ -2,12 +2,14 @@ from pathlib import Path
 from typer import Typer
 from data.download import download_neo4j_text2cypher
 from dotenv import load_dotenv
+from data.preprocess import preprocess_text2cypher
 
 
 def run():
     app = Typer()
     load_dotenv(Path("/home/devel/neo4jam/confidential/.env"))
     app.command(help="Download text2cypher data from HuggingFace")(download_neo4j_text2cypher)
+    app.command(help="Preprocess text2cypher data")(preprocess_text2cypher)
     app()
 
 
---
+++ data/preprocess.py
@@ -0,0 +1,19 @@
+from loguru import logger
+import pandas as pd
+
+
+def preprocess_text2cypher(file: str, dest: str) -> None:
+    df = pd.read_csv(file)
+    logger.info("Read data from {}", file)
+    logger.info("DF summary: {}", df.info())
+
+    # Drop rows with missing DB name
+    df.dropna(subset=["database_reference_alias"], inplace=True)
+    logger.info("Dropped rows with missing DB name")
+
+    # Save the data, one CSV per DB
+    for db in df["database_reference_alias"].unique():
+        db_df = df[df["database_reference_alias"] == db]
+        db_df.to_csv(f"{dest}/{db}.csv", index=False)
+        logger.info("Saved data for {}", db)
+    logger.info("Preprocessing complete")
