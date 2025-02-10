efe21c1888202227f33c82eb7fef35e90ca7a296
Farhan Ahmad <farhanhubble@gmail.com>
2025-02-10 23:11:22 +0800

    FIX: Load env vars on startup

--- cli.py
+++ cli.py
@@ -1,9 +1,12 @@
+from pathlib import Path
 from typer import Typer
 from data.download import download_neo4j_text2cypher
+from dotenv import load_dotenv
 
 
 def run():
     app = Typer()
+    load_dotenv(Path("/home/devel/neo4jam/confidential/.env"))
     app.command(help="Download text2cypher data from HuggingFace")(download_neo4j_text2cypher)
     app()
 
