6f8603f7092e483a6620232d4f849a5ed1c44efc
Farhan Ahmad <farhanhubble@gmail.com>
2025-02-11 15:07:26 +0800

    FEAT: Shorten CLI commands

--- neo4jam/cli.py
+++ neo4jam/cli.py
@@ -5,11 +5,19 @@ from dotenv import load_dotenv
 from data.preprocess import preprocess_text2cypher
 
 
+def download(to: str) -> None:
+    download_neo4j_text2cypher(to)
+
+
+def preprocess(file: str, dest: str) -> None:
+    preprocess_text2cypher(file, dest)
+
+
 def run():
     app = Typer()
     load_dotenv(Path("/home/devel/neo4jam/confidential/.env"))
-    app.command(help="Download text2cypher data from HuggingFace")(download_neo4j_text2cypher)
-    app.command(help="Preprocess text2cypher data")(preprocess_text2cypher)
+    app.command(help="Download text2cypher data from HuggingFace")(download)
+    app.command(help="Preprocess text2cypher data")(preprocess)
     app()
 
 
