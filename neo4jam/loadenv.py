bf7016e4e1768043b80e084c5f74175b67b528af
Farhan Ahmad <farhanhubble@gmail.com>
2025-02-11 15:25:26 +0800

    FIX: Move secrets to `.secrets` dir

--- neo4jam/loadenv.py
+++ neo4jam/loadenv.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 from pathlib import Path
 
 def load_env():
-    load_dotenv(Path("/home/devel/neo4jam/confidential/.env"))
+    load_dotenv(Path("./.secrets/.env"))
     print(os.getenv("GEMINI"))
     print(os.getenv("HUGGING_FACE"))
