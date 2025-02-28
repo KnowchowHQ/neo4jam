9cb8ee861415c597ad5928467b264dd1f78e1416
Farhan Ahmad <farhanhubble@gmail.com>
2025-03-01 00:14:17 +0800

    FIX: Simplify prompt structure

--- neo4jam/prompt.py
+++ neo4jam/prompt.py
@@ -1,89 +1,11 @@
-from __future__ import annotations
-from abc import ABC, abstractmethod
+def system_prompt():
+    return """You are an AI assistant helping a user to write a Cypher 
+        query to retrieve data from a Neo4j database. The user has provided you
+        with a natural language query. You need to generate a Cypher query that
+        retrieves the data the user is asking for."""
 
 
-# Abstract Product (Prompt Builder)
-class PromptBuilder(ABC):
-    @abstractmethod
-    def build_prompt(self, **kargs) -> dict:
-        pass
-
-
-# Concrete Products (Specific Builders)
-class SystemPromptBuilder(PromptBuilder) :
-    def build_prompt(self, **kargs) -> dict:
-        """
-            Generates a structured prompt with components.
-
-            Args: A dictionary with the following keys-
-                role (str): A valid role system/user.
-                context (str): Background information or additional details to aid in the task.
-                input_data (str): The specific input data for the task.
-
-            Returns:
-                dict: A dictionary containing the structured prompt components.
-            """
-
-        print("Running System Prompt builder.")
-        
-        prompt = {}
-
-        for _key in kargs:
-            value = kargs.get(_key, None)
-            if value is not None:
-                prompt.update({_key:value})
-
-        return prompt 
-
-
-class UserPromptBuilder(PromptBuilder):
-    def build_prompt(self, **kargs) -> dict:
-        """
-
-            Generates a structured prompt with components.
-
-            Args: A dictionary with the following keys-
-                role (str): A valid role system/user.
-                context (str): Background information or additional details to aid in the task.
-                input_data (str): The specific input data for the task.
-
-            Returns:
-                dict: A dictionary containing the structured prompt components.
-            """
-
-        print("Running User Prompt builder.")
-        
-        prompt = {}
-
-        for _key in kargs:
-            value = kargs.get(_key, None)
-            if value is not None:
-                prompt.update({_key:value})
-
-        return prompt 
-
-
-# Abstract Factory
-class PromptFactory(ABC):
-    @abstractmethod
-    def create_prompt_builder(self) -> PromptBuilder:
-        pass
-
-
-# Concrete Factories
-class SystemPrompt(PromptFactory):
-    def create_prompt_builder(self) -> PromptBuilder:
-        return SystemPromptBuilder()
-
-class UserPrompt(PromptFactory):
-    def create_prompt_builder(self) -> PromptBuilder:
-        return UserPromptBuilder()
-
-
-
-if __name__ == "__main__":
-    spf = SystemPrompt()
-    print(spf.create_prompt_builder().build_prompt())
-
-    upf = UserPrompt()
-    print(upf.create_prompt_builder().build_prompt())
+def user_prompt(schema: str, question: str):
+    return f"""Following is the Neo4j database schema for the given question.
+            Schema: {schema}
+            User query: {question}"""
