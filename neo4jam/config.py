9c74ccc97cac9cdea3b7b6ae0dfaa2c8cc57d825
Farhan Ahmad <farhanhubble@gmail.com>
2025-03-03 14:58:30 +0800

    FEAT: Module `config`

---
+++ neo4jam/config.py
@@ -0,0 +1,20 @@
+import json
+from pydantic import BaseModel, RootModel, Field
+from pydantic.types import Annotated
+
+
+class Experiments(BaseModel):
+    seed:int = Annotated[int, Field(..., description="Seed for random number generation")]
+
+
+class Config(BaseModel):
+    experiments: Experiments
+
+
+
+with open("config.json", "r") as f:
+    config = Config(**json.load(f))
+
+
+if __name__ == "__main__":
+    print(config.model_dump_json(indent=2))
