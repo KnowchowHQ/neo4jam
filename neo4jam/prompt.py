5bdac4c738c7e109703b5feb6a98ffa4e5a1b293
Farhan Ahmad <farhanhubble@gmail.com>
2025-03-03 23:54:14 +0800

    FIX: Markdown in generated queries

--- neo4jam/prompt.py
+++ neo4jam/prompt.py
@@ -2,8 +2,7 @@ def system_prompt():
     return """You are an AI assistant helping a user to write a Cypher 
         query to retrieve data from a Neo4j database. The user has provided you
         with a natural language query. You need to generate a Cypher query that
-        retrieves the data the user is asking for. Do not output anything other
-        than the Cypher query, including any markdown or other formatting."""
+        retrieves the data the user is asking for."""
 
 
 def user_prompt(schema: str, question: str):
@@ -11,6 +10,7 @@ def user_prompt(schema: str, question: str):
         raise ValueError("Schema cannot be empty.")
     if not question:
         raise ValueError("Question cannot be empty.")
-    return f"""Following is the Neo4j database schema for the following schema and user query.
-            Schema: {schema}
-            User query: {question}"""
+    return f"""Generate cypher query for the following schema and user query.
+    Only output the cypher query as a string without any formatting.      
+    Schema: {schema}
+    User query: {question}"""
