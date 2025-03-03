ec1ff7efedb3db9977cd68cc5e812a99e2c2cb7a
Farhan Ahmad <farhanhubble@gmail.com>
2025-03-03 15:00:26 +0800

    FEAT: Dataset sampling

--- neo4jam/data/preprocess.py
+++ neo4jam/data/preprocess.py
@@ -1,3 +1,4 @@
+from config import config
 from loguru import logger
 import os
 import pandas as pd
@@ -20,3 +21,24 @@ def preprocess_text2cypher(file: str, dest: str) -> None:
         db_df.to_csv(f"{dest}/{db}.csv", index=False)
         logger.info("Saved data for {}", db)
     logger.info("Preprocessing complete")
+
+
+def sample_text2cypher(file: str, dest: str, frac: float | None, n: int | None) -> None:
+    if frac is None and n is None:
+        raise ValueError("Either frac or n must be provided")
+    if frac is not None and n is not None:
+        raise ValueError("Only one of frac or n must be provided")
+
+    df = pd.read_csv(file)
+    logger.info("Read data from {}", file)
+
+    # Sample data
+    if n is not None:
+        sampled_df = df.sample(n=n, random_state=config.experiments.seed)
+    else:
+        sampled_df = df.sample(frac=frac, random_state=config.experiments.seed)
+
+    # Save the data
+    sampled_df.to_csv(dest, index=False)
+    logger.info("Saved sampled data to {}", dest)
+    logger.info("Sampling complete")
