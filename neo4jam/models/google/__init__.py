1a07f4a29298b143ed4b3f27211d258c15d45824
Farhan Ahmad <farhanhubble@gmail.com>
2025-03-03 14:20:33 +0800

    FIX: Gemini key rotation

--- neo4jam/models/google/__init__.py
+++ neo4jam/models/google/__init__.py
@@ -18,7 +18,8 @@ class GeminiAPI:
     def __init__(self, model_name: GEMINI_AVAILABLE_MODELS, system_prompt: str | None = None):
         if model_name not in GEMINI_AVAILABLE_MODELS:
             raise ValueError(f"Model {model_name} is not available. Choose from {[e.value for e in GEMINI_AVAILABLE_MODELS]}")
-        api_keys = [os.environ.get("GEMINI_1"), os.environ.get("GEMINI_2"), os.environ.get("GEMINI_3")]
+        api_keys = [os.environ.get("GEMINI_1")]
+        
         self._key_manager = GeminiAPIKeyManager(api_keys, per_day_quota=1000, max_per_min_quota=15, cool_off_period=60)
 
         self._client = genai.Client(api_key=self._key_manager.get_key())
@@ -40,18 +41,18 @@ class GeminiAPI:
             return response.text
         except ClientError as e:
             logger.warning("Google request quota reached. Waiting...")
-            self._client.update_api_key(self._key_manager.get_key())
+            new_key = self._key_manager.get_key()
+            logger.info(f"Switching to new key: {new_key}")
+            self._client = genai.Client(api_key=new_key)
         finally:
-            response = self._client.models.generate_content(
-                model=self._model,
-                contents=query,
-                config=types.GenerateContentConfig(
-                    system_instruction=self._system_prompt or None
-                ),
-            )
-            return response.text
-
-
+                response = self._client.models.generate_content(
+                    model=self._model,
+                    contents=query,
+                    config=types.GenerateContentConfig(
+                        system_instruction=self._system_prompt or None
+                    ),
+                )
+                return response.text
 
 class GeminiAPIKeyManager:
     def __init__(self, keys, per_day_quota=500, max_per_min_quota=15, cool_off_period=60):
@@ -60,17 +61,19 @@ class GeminiAPIKeyManager:
         self.max_per_min_quota = max_per_min_quota
         self.cool_off_period = cool_off_period
         self.usage_count = 0
-        self.day_start_time = time.time()
-        self.min_start_time = time.time()
+        self.day_start_time = time()
+        self.min_start_time = time()
         self.key_usage = {key: {'min': 0, 'day': 0, 'last_used': 0} for key in keys}
 
     def get_key(self):
+        logger.debug(f"Usage count: {self.usage_count}")
+        logger.debug(f"Key usage: {self.key_usage}")
         # Check if it's a new day
-        if time.time() - self.day_start_time > 24 * 60 * 60:
+        if time() - self.day_start_time > 24 * 60 * 60:
             self.reset_daily_usage()
 
         # Check if it's a new minute
-        if time.time() - self.min_start_time > 60:
+        if time() - self.min_start_time > 60:
             self.reset_minute_usage()
 
         # Rotate through keys
@@ -80,10 +83,10 @@ class GeminiAPIKeyManager:
         # Check if the current key can be used
         if (self.key_usage[current_key]['min'] < self.max_per_min_quota and
             self.key_usage[current_key]['day'] < self.per_day_quota and
-            time.time() - self.key_usage[current_key]['last_used'] >= self.cool_off_period):
+            time() - self.key_usage[current_key]['last_used'] >= self.cool_off_period):
             self.key_usage[current_key]['min'] += 1
             self.key_usage[current_key]['day'] += 1
-            self.key_usage[current_key]['last_used'] = time.time()
+            self.key_usage[current_key]['last_used'] = time()
             self.usage_count += 1
             return current_key
         else:
@@ -93,24 +96,24 @@ class GeminiAPIKeyManager:
                 self.keys.append(current_key)
                 if (self.key_usage[current_key]['min'] < self.max_per_min_quota and
                     self.key_usage[current_key]['day'] < self.per_day_quota and
-                    time.time() - self.key_usage[current_key]['last_used'] >= self.cool_off_period):
+                    time() - self.key_usage[current_key]['last_used'] >= self.cool_off_period):
                     self.key_usage[current_key]['min'] += 1
                     self.key_usage[current_key]['day'] += 1
-                    self.key_usage[current_key]['last_used'] = time.time()
+                    self.key_usage[current_key]['last_used'] = time()
                     self.usage_count += 1
                     return current_key
 
             # If no key is available, wait until the next minute
-            time.sleep(60 - (time.time() - self.min_start_time))
+            sleep(60) # - (time() - self.min_start_time))
             return self.get_key()
 
     def reset_minute_usage(self):
-        self.min_start_time = time.time()
+        self.min_start_time = time()
         for key in self.keys:
             self.key_usage[key]['min'] = 0
 
     def reset_daily_usage(self):
-        self.day_start_time = time.time()
+        self.day_start_time = time()
         for key in self.keys:
             self.key_usage[key]['day'] = 0
 
