0ccceac0ca23c085ea6ce31c4fccdd955df77c5c
Safwan Ahmad <safwan.pico@gmail.com>
2025-03-12 04:01:18 +0800

    FIX: Ensure file handlers are closed.

--- neo4jam/deepeval/deepeval.py
+++ neo4jam/deepeval/deepeval.py
@@ -14,11 +14,17 @@ from tqdm import tqdm
 import multiprocessing
 
 
-def evaluate_generated_cyphers(file_real: str, file_gen: str, rouge_metric, bleu_metric) -> dict:
-    tqdm.pandas(desc="Evaluate execution results for Cyphers")        
-    data_real = json.load(open(file_real))
-    data_gen = json.load(open(file_gen))
-    rougue_score = rouge_metric.calculate(predictions=[data_gen], references=[data_real])
+def evaluate_generated_cyphers(
+    file_real: str, file_gen: str, rouge_metric, bleu_metric
+) -> dict:
+    tqdm.pandas(desc="Evaluate execution results for Cyphers")
+    with open(file_real, "r") as f:
+        data_real = f.read()
+    with open(file_gen, "r") as f:
+        data_gen = f.read()
+    rougue_score = rouge_metric.calculate(
+        predictions=[data_gen], references=[data_real]
+    )
     bleu_score = bleu_metric.calculate(predictions=[data_gen], references=[data_real])
     return {"rouge": rougue_score, "bleu": bleu_score}
 
@@ -52,13 +58,15 @@ def deepeval(input_dir: DirectoryPath, output_dir: DirectoryPath) -> None:
 
     optimal_cores = max(1, multiprocessing.cpu_count() - 1)
     logger.info("Using {} cores for evaluation", optimal_cores)
+
+    def job_generator():
+        for real, generated in data:
+            yield (real, generated, output_dir, rouge_metric, bleu_metric)
+
     with Pool(optimal_cores) as pool:
         pool.starmap(
             __deepeval__,
-            [
-                (real, generated, output_dir, rouge_metric, bleu_metric)
-                for real, generated in data
-            ],
+            job_generator(),
         )
 
     logger.info("Evaluation complete.")
@@ -71,8 +79,9 @@ def __deepeval__(
     rouge_metric,
     bleu_metric,
 ) -> None:
+    logger.debug("Evaluating {} and {}", real_file, gen_file)
     result = evaluate_generated_cyphers(real_file, gen_file, rouge_metric, bleu_metric)
-    
+
     # Save the updated dataframe to a new json file
     filename = Path(real_file).name
 
@@ -80,9 +89,9 @@ def __deepeval__(
 
     # Save the result to respective files
     os.makedirs(directory, exist_ok=True)
-    with open (f"{directory}/{filename}", "w") as f:
+    with open(f"{directory}/{filename}", "w") as f:
         json.dump(result, f, indent=4)
-    
+
     logger.info("Appended scores to {}", f"{directory}/{filename}")
 
 
@@ -90,4 +99,4 @@ if __name__ == "__main__":
     deepeval(
         input_dir=Path("/home/devel/neo4jam/data/text2cypher/deepgen"),
         output_dir=Path("/home/devel/neo4jam/data/text2cypher/deepeval"),
-   )
+    )
